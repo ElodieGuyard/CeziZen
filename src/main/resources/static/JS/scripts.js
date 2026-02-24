0e597e1bf43c0a9fe49c1689ee84f0086c8b36ea
ElodieGuyard <elodieg29@gmail.com>
2026-02-24 07:21:56 +0100

    Add files via upload

---
+++ src/main/resources/static/JS/scripts.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('.menu');
+const toggle = document.querySelector('.menu-toggle');
+
+toggle.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
