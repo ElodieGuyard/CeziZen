f2517553b2cd9ef1d8f20ee07ac39a2ff846fdab
Guyard Elodie <guyard.elodie@outlook.fr>
2026-03-11 11:38:25 +0100

    Fix: replace class by interface in the user repository

--- src/main/java/com/example/cesizen/repository/UserRepository.java
+++ src/main/java/com/example/cesizen/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.example.cesizen.repository;
 import com.example.cesizen.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
 
-public class UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends CrudRepository<User, Integer> {
 
 }
