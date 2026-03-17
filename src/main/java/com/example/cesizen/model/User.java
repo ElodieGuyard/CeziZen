c5ad83231b091774a0d04e4ff83f25ff067b68d3
Guyard Elodie <guyard.elodie@outlook.fr>
2026-03-17 10:29:28 +0100

    Added : all getters and setters for User Model

--- src/main/java/com/example/cesizen/model/User.java
+++ src/main/java/com/example/cesizen/model/User.java
@@ -3,6 +3,8 @@ package com.example.cesizen.model;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 
+import java.util.Date;
+
 @Entity
 @Table(name = "utilisateur")
 public class User {
@@ -13,6 +15,12 @@ public class User {
 
     private String nom_utilisateur;
     private Role role;
+    private String mot_de_passe;
+    private Date cree_le;
+    private Date derniere_connexion;
+    private Date modifie_le;
+    private Date deleted_at;
+
 
     public User() {}
 
@@ -22,6 +30,10 @@ public class User {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getMDP(Integer id){
+        return this.mot_de_passe;
     }
 
     public String getName() {
@@ -38,6 +50,38 @@ public class User {
 
     public Role setRole(Integer id){
         return this.role;
+    }
+
+    public Date getCree_le() {
+        return cree_le;
+    }
+
+    public void setCree_le(Date cree_le) {
+        this.cree_le = cree_le;
+    }
+
+    public Date getDerniere_connexion() {
+        return derniere_connexion;
+    }
+
+    public void setDerniere_connexion(Date derniere_connexion) {
+        this.derniere_connexion = derniere_connexion;
+    }
+
+    public Date getModifie_le() {
+        return modifie_le;
+    }
+
+    public void setModifie_le(Date modifie_le) {
+        this.modifie_le = modifie_le;
+    }
+
+    public Date getDeleted_at() {
+        return deleted_at;
+    }
+
+    public void setDeleted_at(Date deleted_at) {
+        this.deleted_at = deleted_at;
     }
 }
 
