0f3ee1cf8c0c658a000441bb0eddf94247e8911a
Guyard Elodie <guyard.elodie@outlook.fr>
2026-03-19 19:26:06 +0100

    Feat: Respiration webPage created

--- src/main/java/com/example/cesizen/controller/RespirationController.java
+++ src/main/java/com/example/cesizen/controller/RespirationController.java
@@ -1,4 +1,12 @@
 package com.example.cesizen.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class RespirationController {
+    @GetMapping("/respiration")
+    public String viewRespiration() {
+        return "respiration";
+    }
 }
