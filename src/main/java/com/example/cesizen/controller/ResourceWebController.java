67db60490897e0a92145e23153698b22e50e4644
ElodieGuyard <elodie.guyard@decathlon.com>
2026-03-23 13:13:08 +0100

    WIP ressource display

--- src/main/java/com/example/cesizen/controller/ResourceWebController.java
+++ src/main/java/com/example/cesizen/controller/ResourceWebController.java
@@ -1,11 +1,15 @@
 package com.example.cesizen.controller;
 
+import com.example.cesizen.model.Resource;
 import com.example.cesizen.repository.ResourceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path="/ressources")
@@ -18,5 +22,16 @@ public class ResourceWebController {
     public String showRessources(Model model) {
         model.addAttribute("ressources", resourceRepository.findAll());
         return "ressources";
+    }
+
+    @GetMapping(path= "/{id}")
+    public String showSingleRessources(Model model, @PathVariable String id){
+        Optional<Resource> resOpt = resourceRepository.findById(Integer.valueOf(id));
+        if (resOpt.isEmpty()){
+            return "error";
+        } else {
+            model.addAttribute("ressource", resOpt.get());
+            return  "ressource";
+        }
     }
 }
