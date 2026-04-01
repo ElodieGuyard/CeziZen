4ec3b12d1bf38cf85d9ae1195133f2d377900d80
Guyard Elodie <guyard.elodie@outlook.fr>
2026-04-01 14:55:30 +0200

    Feat: The home page now list ressources

--- src/main/java/com/example/cesizen/controller/HomeController.java
+++ src/main/java/com/example/cesizen/controller/HomeController.java
@@ -1,10 +1,16 @@
 package com.example.cesizen.controller;
 
+import com.example.cesizen.repository.ResourceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private ResourceRepository resourceRepository;
 
     @GetMapping("/")
     public String root() {
@@ -12,8 +18,8 @@ public class HomeController {
     }
 
     @GetMapping("/home")
-    public String viewHome(){
+    public String viewHome(Model model){
+        model.addAttribute("ressources", resourceRepository.findAll());
         return "home";
     }
-
 }
