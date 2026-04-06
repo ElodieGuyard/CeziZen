e819de355765a27c4844039cbf224d64cef26ec7
Guyard Elodie <guyard.elodie@outlook.fr>
2026-04-06 21:06:46 +0200

    Fix: Id long for correspond to BigInt in db

--- src/main/java/com/example/cesizen/model/User.java
+++ src/main/java/com/example/cesizen/model/User.java
@@ -11,7 +11,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
     @Column(nullable = false)
     private String nom;
     @Enumerated(EnumType.STRING)
@@ -43,11 +43,11 @@ public class User {
         this.mot_de_passe = mot_de_passe;
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
