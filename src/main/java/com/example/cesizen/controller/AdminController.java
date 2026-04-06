f2ee81778a8a820301bb1f1287919a75ca01390a
Guyard Elodie <guyard.elodie@outlook.fr>
2026-04-06 21:31:43 +0200

    Added feat user administration pages

--- src/main/java/com/example/cesizen/controller/AdminController.java
+++ src/main/java/com/example/cesizen/controller/AdminController.java
@@ -1,12 +1,13 @@
 package com.example.cesizen.controller;
 
+import com.example.cesizen.model.Role;
+import com.example.cesizen.model.User;
 import com.example.cesizen.repository.ResourceRepository;
 import com.example.cesizen.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(path="/admin")
@@ -16,6 +17,11 @@ public class AdminController {
     private ResourceRepository resourceRepository;
     @Autowired
     private UserRepository userRepository;
+
+    @GetMapping({"", "/"})
+    public String adminHome() {
+        return "redirect:/admin/dashboard";
+    }
 
     @GetMapping("/dashboard")
     public String admin() {
@@ -32,5 +38,32 @@ public class AdminController {
     public String showUsers(Model model) {
         model.addAttribute("users", userRepository.findAll());
         return "admin/users";
+    }
+
+    @GetMapping("/users/{id}/edit")
+    public String editUser(@PathVariable Long id, Model model) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("User not found id=" + id));
+
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values()); // pour remplir le <select>
+        return "admin/user_edit";
+    }
+
+    @PostMapping("/users/{id}/edit")
+    public String updateUser(
+            @PathVariable Long id,
+            @RequestParam Role role,
+            @RequestParam(name = "enabled", defaultValue = "false") boolean enabled
+    ) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("User not found id=" + id));
+
+        user.setRole(role);
+        if (enabled) user.setEnabled();
+        else user.setDisable();
+
+        userRepository.save(user);
+        return "redirect:/admin/users";
     }
 }
