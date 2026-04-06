699df743bb536bacdf24c5cb2849c45f9a0c9472
Guyard Elodie <guyard.elodie@outlook.fr>
2026-04-06 20:28:16 +0200

    Refactor: views admin pages, started admin controller

--- src/main/java/com/example/cesizen/controller/AdminController.java
+++ src/main/java/com/example/cesizen/controller/AdminController.java
@@ -1,7 +1,25 @@
 package com.example.cesizen.controller;
 
+import com.example.cesizen.repository.ResourceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
 public class AdminController {
+
+    @Autowired
+    private ResourceRepository resourceRepository;
+
     public String admin() {
         return "dashboard";
+    }
+
+    @GetMapping
+    public String showRessources(Model model) {
+        model.addAttribute("ressources", resourceRepository.findAll());
+        return "admin/ressources";
     }
 }
